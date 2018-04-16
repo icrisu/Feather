558131b47e3f823b0d5f11bd460d542809fe7144
Ionel Crisu <crisu.ionel@gmail.com>
2018-04-16 12:13:33 +0300

    Add node modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import reducers from './reducers';
+
 import './styles/css/index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const store = createStore(reducers, {}, compose(applyMiddleware(ReduxThunk)));
+
+ReactDOM.render(
+<Provider store={ store }>
+    <App />
+</Provider>, document.getElementById('root'));
